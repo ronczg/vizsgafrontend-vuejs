efb4efb40643e96bf4d376b3120e775c970279f6
ronczg <roncz.gabor1@gmail.com>
2022-03-29 11:18:08 +0200

    openpagecomponens

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
+
+const app = createApp(App)
 import 'bootstrap/dist/js/bootstrap'
 
-createApp(App).mount('#app')
+app.mount('#app')
