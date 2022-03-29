c86d745b3e130c7a4b74815c2061bd68d3af6731
ronczg <roncz.gabor1@gmail.com>
2022-03-29 11:34:03 +0200

    routing

--- hahu-app/src/main.js
+++ hahu-app/src/main.js
@@ -1,8 +1,21 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import OpenPage from './components/OpenPage'
+import IngatlanKinalat from './components/IngatlanKinalat'
+
+import { createRouter, createWebHistory} from 'vue-router'
+
 import 'bootstrap/dist/css/bootstrap.css'
 
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {path: '/', component: OpenPage},
+        {path: '/offers', component: IngatlanKinalat}
+    ]
+})
 const app = createApp(App)
+app.use(router)
 import 'bootstrap/dist/js/bootstrap'
 
 app.mount('#app')
